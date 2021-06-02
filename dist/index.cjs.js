ec63fd1eea41315ffac7af50d198d456f5b58869
saraahso <saraahso@gmail.com>
2021-06-02 18:17:27 +0200

    Basic Library Files

---
+++ dist/index.cjs.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+function HelloWorld() {
+  return /*#__PURE__*/React__default['default'].createElement("h1", null, "Hello World");
+}
+
+exports.HelloWorld = HelloWorld;
---
+++ dist/index.esm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HelloWorld() {
+  return /*#__PURE__*/React.createElement("h1", null, "Hello World");
+}
+
+export { HelloWorld };
